cd23bffdeee64375a40d07133635f44db28c6ac9
agunggantengdh <agunggantengdh@gmail.com>
2025-08-14 16:23:22 +0000

    add diff

---
+++ diff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Mengecek apakah ada parameter yang diberikan untuk pesan
+message="${1:-generate commit message}"
+
+# Mendapatkan informasi tentang perubahan, penambahan, dan penghapusan file
+status_output=$(git status --short)
+diff_output=$(git diff)
+
+clear
+
+# Gabungkan hasil status dan diff
+if [ -z "$diff_output" ] && [ -z "$status_output" ]; then
+  echo -e "$message"
+else
+  echo -e "$message\n\nStatus Perubahan:\n$status_output\n\nGit Diff:\n$diff_output"
+fi
+
+echo "Git diff, status, and message have been copied to clipboard."
